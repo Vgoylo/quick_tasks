5cfc012ad09bf74829756d58a129718e015543fd
vgoylo <y_draiv.by@mail.ru>
2021-10-07 16:50:10 +0300

    Refactor

--- get_even.rb
+++ get_even.rb
@@ -1,25 +1,25 @@
-# def even(arr)
-#   arr.map do |element|
-#     if element % 2 == 0
-#       element
-#     end
-#   end.compact
-# end
+def even(arr)
+  arr.map do |element|
+    if element % 2 == 0
+      element
+    end
+  end.compact
+end
 
-# p even([1, 2, 3, 4, 5, 6, 7, 8, 9, 35, 7, 84, 778])
+p even([1, 2, 3, 4, 5, 6, 7, 8, 9, 35, 7, 84, 778])
 
 
-# def even(arr)
-#   new_arr = []
-#    arr.each do |element|
-#     if element % 2 == 0
-#      new_arr << element 
-#     end
-#   end
-#   new_arr 
-# end
+def even(arr)
+  new_arr = []
+   arr.each do |element|
+    if element % 2 == 0
+     new_arr << element 
+    end
+  end
+  new_arr 
+end
 
-# p even([1, 2, 3, 4, 5, 6, 7, 8, 9, 35, 7, 84, 778])
+p even([1, 2, 3, 4, 5, 6, 7, 8, 9, 35, 7, 84, 778])
 
 
 def even(arr)
