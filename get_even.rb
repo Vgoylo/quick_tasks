56b709765b1a1b7ed1922f85608abc957278e2fa
vgoylo <y_draiv.by@mail.ru>
2021-10-07 16:40:29 +0300

    Add get_even numbers from an array using each

--- get_even.rb
+++ get_even.rb
@@ -1,9 +1,22 @@
+# def even(arr)
+#   arr.map do |element|
+#     if element % 2 == 0
+#       element
+#     end
+#   end.compact
+# end
+
+# p even([1, 2, 3, 4, 5, 6, 7, 8, 9, 35, 7, 84, 778])
+
+
 def even(arr)
-  arr.map do |element|
+  new_arr = []
+   arr.each do |element|
     if element % 2 == 0
-      element
+     new_arr << element 
     end
-  end.compact
+  end
+  new_arr 
 end
 
 p even([1, 2, 3, 4, 5, 6, 7, 8, 9, 35, 7, 84, 778])
