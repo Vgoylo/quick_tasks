0f781782442f996abd5dd446c82e727cf2a8820e
vgoylo <y_draiv.by@mail.ru>
2021-10-07 16:43:48 +0300

    Add get_even numbers from an array using select

--- get_even.rb
+++ get_even.rb
@@ -9,14 +9,22 @@
 # p even([1, 2, 3, 4, 5, 6, 7, 8, 9, 35, 7, 84, 778])
 
 
+# def even(arr)
+#   new_arr = []
+#    arr.each do |element|
+#     if element % 2 == 0
+#      new_arr << element 
+#     end
+#   end
+#   new_arr 
+# end
+
+# p even([1, 2, 3, 4, 5, 6, 7, 8, 9, 35, 7, 84, 778])
+
+
 def even(arr)
-  new_arr = []
-   arr.each do |element|
-    if element % 2 == 0
-     new_arr << element 
-    end
-  end
-  new_arr 
+  #arr.select {|element| element % 2 == 0}
+  arr.select(&:even?)
 end
 
 p even([1, 2, 3, 4, 5, 6, 7, 8, 9, 35, 7, 84, 778])
