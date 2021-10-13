1dbddfb1585b1ae7aae21f3a99eefb0577d46813
vgoylo <y_draiv.by@mail.ru>
2021-10-13 11:08:20 +0300

    Refactor run5.rb

--- run5.rb
+++ run5.rb
@@ -1,13 +1,13 @@
-def sort_key(hash)
+def sort_keys(hash)
   Hash[hash.sort] # sprt by keys
 end
 
-p sort_key({5 => 'aaaaa', 6 => 'fffff', 8 => 'bbbbb', 3 => 'cccccc'})
+p sort_keys({5 => 'aaaaa', 6 => 'fffff', 8 => 'bbbbb', 3 => 'cccccc'})
 
 
-def sort_key(hash)
+def sort_values(hash)
   Hash[hash.sort_by{ |k,v| v}] # sort by values
 end
 
 
-p sort_key({1 => 'aaaaa', 4 => 'dddd', 9 => 'rrrrr', 3 => 'cccccc'})
+p sort_values({1 => 'aaaaa', 4 => 'dddd', 9 => 'rrrrr', 3 => 'cccccc'})
