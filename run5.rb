cfed6bdc3a1312405df5d3ade4dda78e7f5c4c72
vgoylo <y_draiv.by@mail.ru>
2021-10-15 11:10:48 +0300

    Add my_even?

--- run5.rb
+++ run5.rb
@@ -11,3 +11,15 @@ end
 
 
 p sort_values({1 => 'aaaaa', 4 => 'dddd', 9 => 'rrrrr', 3 => 'cccccc'})
+
+
+def my_even?(arr)
+  arr.select do |element|
+    if element % 2 == 0
+      element
+    end
+  end
+  #arr.select{|element| element.enve?}
+end
+
+p my_even?([1,2,3,4,5,6,7,8,9])
